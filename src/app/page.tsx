542fd4491e5abc361d8230d325fc33285bed9e40
amin-mehr <amin.mehravar@yahoo.com>
2025-09-06 09:33:29 +0330

    first init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 export default function Home() {
-  console.log('test');
+  //console.log('test');
 
-  const renderName = () => {
+  const renderName = (name2: any) => {
     const name = 'Amin';
     return <span>{name}</span>;
   };
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <div className="">my first page</div>
-      {renderName()}
+      {renderName(1)}
     </div>
   );
 }
